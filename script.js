ba269fe3297121c186fde18ba39e0cb611776ee2
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-08-21 18:42:53 +0600

    discount

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 let total = 0.0;
+let discount = 0.0;
+
+function discountCalc(total) {
+  const couponInput = document.getElementById("couponInput");
+  const coupon = couponInput.value;
+
+  if (coupon === "SELL200") {
+    return total * 0.2; // 20% discount
+  } else {
+    return 0;
+  }
+}
+
 function getCardInfo(data) {
   const name = data.childNodes[3].childNodes[3].innerText;
   const price = data.childNodes[3].childNodes[5].innerText.split(" ")[0];
@@ -14,4 +27,13 @@ function getCardInfo(data) {
 
   total = parseFloat(total) + parseFloat(price);
   totalPrice.innerText = total.toFixed(2);
+  grandTotal.innerText = total.toFixed(2);
+
+  discount = discountCalc(total);
+  discountPrice.innerText = discount.toFixed(2);
+
+  const finalTotal = total - discount;
+  grandTotal.innerText = finalTotal.toFixed(2);
+
+  console.log(discount);
 }
